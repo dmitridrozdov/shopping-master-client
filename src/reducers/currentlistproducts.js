bc9f5cb4f17a6a9edeb810e11aa3825c33b231b4
dmitridrozdov <dm.drozdov@gmail.com>
2022-08-24 16:28:33 +1000

    Update currentlistproducts.js

--- src/reducers/currentlistproducts.js
+++ src/reducers/currentlistproducts.js
@@ -1,21 +1,4 @@
-import { CREATE, UPDATE, DELETE, FETCH_ALL, FETCH_FULL } from '../constants/actionTypes'
-
-// export default (products = [], action) => {
-//     switch (action.type) {
-//         case DELETE:
-//             return products.filter((products) => products._id !== action.payload)
-//         case UPDATE:
-//             return products.map((products) => products._id === action.payload._id ? action.payload : products)
-//         case FETCH_ALL:
-//             return action.payload
-//         case FETCH_FULL:
-//             return action.payload
-//         case CREATE:
-//             return [...products, action.payload]
-//         default:
-//             return products
-//     }
-// }
+import { CREATE, UPDATE, DELETE, FETCH_ALL } from '../constants/actionTypes'
 
 export default (currentlistproducts = [], action) => {
     switch (action.type) {
@@ -24,8 +7,6 @@ export default (currentlistproducts = [], action) => {
         case UPDATE:
             return currentlistproducts.map((currentlistproducts) => currentlistproducts._id === action.payload._id ? action.payload : currentlistproducts)
         case FETCH_ALL:
-            return action.payload
-        case FETCH_FULL:
             return action.payload
         case CREATE:
             return [...currentlistproducts, action.payload]
