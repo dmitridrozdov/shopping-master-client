9f91fadcad064f8d173913bd2e5a4ea5711826f3
dmitridrozdov <dm.drozdov@gmail.com>
2022-09-05 17:15:23 +1000

    Update MyBar.js

--- src/components/AppBar/MyBar.js
+++ src/components/AppBar/MyBar.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import useStyles from './styles'
 import { useSelector } from 'react-redux'
-import { Typography, AppBar, Toolbar,IconButton } from '@material-ui/core'
-import Add from '@material-ui/icons/Add'
+import { Typography, AppBar, Toolbar, /*IconButton*/ } from '@material-ui/core'
+// import Add from '@material-ui/icons/Add'
 
 const MyBar = () => {
     const classes = useStyles()
@@ -10,9 +10,9 @@ const MyBar = () => {
     return (
         <AppBar className = {classes.appBar} position='sticky' color='inherit' >
             <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                     <Add />
-                </IconButton>
+                </IconButton> */}
                 <Typography className={classes.headerTextStyle}>Shopping List ({currentListProductsLength})</Typography>
             </Toolbar>
         </AppBar>
