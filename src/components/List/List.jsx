07af3ed19d5f550ca730a594d06907e024dfc17c
dmitridrozdov <dm.drozdov@gmail.com>
2022-09-05 17:15:32 +1000

    Update List.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,15 +2,11 @@ import React from 'react'
 import { Fade, List as MUList } from '@material-ui/core'
 import useStyles from './styles'
 import { useSelector } from 'react-redux'
-// import { deleteProduct } from '../../actions/products'
-// import { useDispatch } from 'react-redux'
-import ListItemCustom from './ListItem/ListItem'
 import Category from './Category/Category'
 
 const List = () => {
     const classes = useStyles()
     const currentListProducts = useSelector((state) => state.currentlistproducts)
-    // const dispatch = useDispatch()
 
     const transformToCategoryProductStructure = () => {
         const onlyUnique = (value, index, self) => {
@@ -23,14 +19,9 @@ const List = () => {
 
     return (
         <MUList className={classes.list}>
-            {/* {currentListProducts.map((product) => (
-                <Fade in={true} key={product._id}>
-                    <ListItemCustom id={product._id} product={product.product}/>
-                </Fade>
-            ))} */}
             {categoriesWithProducts.map(c => (
-                <Fade in={true} key={c._id}>
-                    <Category id={c._id} category={c}/>
+                <Fade in={true} key={c}>
+                    <Category key={c} category={c}/>
                 </Fade>
             ))}
         </MUList>
