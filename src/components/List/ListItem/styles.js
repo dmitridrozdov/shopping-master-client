00ad04bc3baf1480a568ce140436bd2162fd4461
dmitridrozdov <dm.drozdov@gmail.com>
2022-09-28 15:51:43 +1000

    Update styles.js

--- src/components/List/ListItem/styles.js
+++ src/components/List/ListItem/styles.js
@@ -2,25 +2,20 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
 
-  // listItem: {
-  //   "&,&:focus": {
-  //     backgroundColor: theme.palette.background.paper
-  //   }
-  // },
-
-  listItemDone: {
-    "&,&:focus": {
-      backgroundColor: "#cc0000",
-      color: "white"
-    },
-  },
-
   listItem: { 
     display: 'inline-block',
     padding: '5px 5px',
     fontFamily: 'Montserrat',
-    fontSize: '10px',
-    color: '#858585',
+    fontSize: '12px',
+    cursor: 'pointer',
    },
+
+   overlay: {
+    position: 'absolute',
+    top: '15px',
+    right: '10px',
+    fontFamily: 'Montserrat',
+    fontSize: '12px',
+  },
 
 }));
