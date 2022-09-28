018535d2d25b044ff6736bc1abb7cc2be78c2747
dmitridrozdov <dm.drozdov@gmail.com>
2022-09-28 10:24:01 +1000

    Update ListItem.jsx

--- src/components/List/ListItem/ListItem.jsx
+++ src/components/List/ListItem/ListItem.jsx
@@ -1,26 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ListItem, ListItemText, Typography } from '@material-ui/core'
 import { deleteProduct } from '../../../actions/products'
 import { useDispatch } from 'react-redux'
 import useStyles from './styles'
 
+const woolisApi = {
+    base: "https://www.woolworths.com.au/api/v3/ui/schemaorg/product/",
+  }
+
 const ListItemCustom = ({ id, product }) => {
     const classes = useStyles()
     const dispatch = useDispatch()
     const [done, setDone] = useState(false)
+    const [price, setPrice] = useState(0)
     
     const onClickItem = (id) => {
         dispatch(deleteProduct(id))
         setDone(true)
     }
 
+    const productID = '231598'
+
+
+    if(price === 0) {
+        fetch(`${woolisApi.base}` + productID)
+          .then((res) => res.json())
+          .then((result) => {
+            setPrice(result.offers.price)
+          })
+          console.log(product)
+    }
+
     return (
-        // <ListItem divider={true} classes={{root: classes.divider}}>
         <ListItem divider={true} className={done ? classes.listItemDone : classes.listItem}>
             <ListItemText
                 disableTypography
-                primary={<Typography type="body2" style={{ fontFamily: 'Montserrat', fontSize: '12px', cursor: 'pointer' }}>{product}</Typography>}
-                // onClick={() => dispatch(deleteProduct(id))}/>
+                primary={
+                        <div>
+                            <Typography type="body2" style={{ fontFamily: 'Montserrat', fontSize: '12px', cursor: 'pointer' }}>
+                                {product}
+                            </Typography>
+                            <Typography>{price}</Typography>    
+                        </div>
+                        
+                    }
                 onClick={() => onClickItem(id)}/>
         </ListItem>
     )
