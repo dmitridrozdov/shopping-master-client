28c8eb0d067262150795255b5d3713305fff7a52
dmitridrozdov <dm.drozdov@gmail.com>
2022-09-29 15:49:33 +1000

    Update ListItem.jsx

--- src/components/List/ListItem/ListItem.jsx
+++ src/components/List/ListItem/ListItem.jsx
@@ -8,27 +8,30 @@ const woolisApi = {
     base: "https://www.woolworths.com.au/api/v3/ui/schemaorg/product/",
   }
 
-const ListItemCustom = ({ id, product }) => {
+const ListItemCustom = ({ id, product, wid }) => {
     const classes = useStyles()
     const dispatch = useDispatch()
     const [done, setDone] = useState(false)
     const [price, setPrice] = useState(0)
+    const [productName, setProductName] = useState('')
     
     const onClickItem = (id) => {
         dispatch(deleteProduct(id))
         setDone(true)
     }
 
-    const productID = '231598'
+    // const productID = '231598'
 
-
-    if(price === 0) {
-        fetch(`${woolisApi.base}` + productID)
-          .then((res) => res.json())
-          .then((result) => {
-            setPrice(result.offers.price)
-          })
-          console.log(product)
+    if(wid !== '') {
+        console.log(wid)
+        if(price === 0) {
+            fetch(`${woolisApi.base}` + wid)
+              .then((res) => res.json())
+              .then((result) => {
+                setPrice(result.offers.price)
+                setProductName(result.name)
+              })
+        }
     }
 
     return (
