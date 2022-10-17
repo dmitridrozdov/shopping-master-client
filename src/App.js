1ed948e0e010b0f4f906da92e6004074fd3e96c5
dmitridrozdov <dm.drozdov@gmail.com>
2022-10-17 15:12:59 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,66 +1,3 @@
-// import React, { useEffect, useRef } from 'react'
-// import { useDispatch } from 'react-redux'
-// import { Container, Grow } from '@material-ui/core'
-// // import useStyles from './styles'
-// import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui'
-// import { SpeechState, useSpeechContext } from '@speechly/react-client'
-// import { getProducts } from './actions/products'
-// import { getALLProducts } from './actions/products'
-
-// import AddProductToCurrentListForm from './components/AddProductForm/AddProductToCurrentListForm'
-// import List from './components/List/List'
-// import MyBar from './components/AppBar/MyBar'
-
-// const App = () => {
-//     // const classes = useStyles()
-//     const { speechState } = useSpeechContext()
-//     const main = useRef(null)
-//     const dispatch = useDispatch()
-
-//     const executeScroll = () => main.current.scrollIntoView()
-
-//     useEffect(() => {
-//         if(speechState === SpeechState.Recording) {
-//             executeScroll()
-//         }
-//     }, [speechState])
-
-//     useEffect(() => {
-//         dispatch(getProducts())
-//     }, [dispatch])
-
-//     useEffect(() => {
-//         dispatch(getALLProducts())
-//     }, [dispatch])
-
-//     return (
-//         <Container maxWidth='sm'>
-
-//             <MyBar />
-
-//             <Grow in>
-//                 <Container>
-//                     <AddProductToCurrentListForm position='sticky' className='not-scrolled'/>
-//                 </Container>
-//             </Grow>
-
-//             <Grow in>
-//                 <Container ref={main}>
-//                     <List />
-//                 </Container>
-//             </Grow>
-
-//             <PushToTalkButtonContainer>
-//                 <PushToTalkButton />
-//                 <ErrorPanel />
-//             </PushToTalkButtonContainer>
-
-//         </Container>
-//     )
-// }
-
-// export default App
-
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Container } from '@material-ui/core'
@@ -74,7 +11,7 @@ const App = () => {
             <Container maxWidth='xl'>
                 <Routes>
                     <Route path='/' exact element={<AppList />} />
-                    <Route path='/alerts' exact component={<Alerts />} />
+                    <Route path='/alerts' exact element={<Alerts />} />
                 </Routes>
             </Container>
         </BrowserRouter>
