f7d045bdcb734d5fb43010ac63cecedb2cea4f77
dmitridrozdov <dm.drozdov@gmail.com>
2022-10-17 15:13:04 +1100

    Update Alerts.js

--- src/components/Alerts/Alerts.js
+++ src/components/Alerts/Alerts.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import useStyles from './styles'
+import { Typography, AppBar, Toolbar, IconButton } from '@material-ui/core'
+import ArrowBack from '@material-ui/icons/ArrowBackIosOutlined'
 
 const Alerts = () => {
+  const classes = useStyles()
   return (
-    <div>Alerts</div>
+    <div>
+      <IconButton href='/' edge="start" className={classes.backButton} color="inherit" aria-label="menu">
+        <ArrowBack />
+      </IconButton>
+      Alerts
+    </div>
   )
 }
 
