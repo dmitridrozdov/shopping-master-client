6e5a83446a7f87226154f40f95c552d8c119ed26
dmitridrozdov <dm.drozdov@gmail.com>
2022-11-02 13:20:17 +1100

    Update Alerts.js

--- src/components/Alerts/Alerts.js
+++ src/components/Alerts/Alerts.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useStyles from './styles'
 import { Fade, List as MUList, IconButton } from '@material-ui/core'
 import ArrowBack from '@material-ui/icons/ArrowBackIosOutlined'
 import AlertListItem from './AlertListItem/AlertListItem'
 
-const Alerts = ({ alerts }) => {
+const woolisApi = {
+  base: 'https://www.woolworths.com.au/api/v3/ui/schemaorg/product/',
+}
+
+const productsToMonitor = [
+  { 
+      wid: '231598',
+      price: '18',
+  },
+  {
+      wid: '160945',
+      price: '26',
+  },
+  { 
+      wid: '813898',
+      price: '5',
+  },
+]
+
+const Alerts = () => {
   const classes = useStyles()
-  console.log(alerts)
+  const [alerts, setAlerts] = useState([])
+
+  const fetchAllWoolisProducts = () => {
+    const promises = productsToMonitor.map(p => {
+      return fetch(`${woolisApi.base}` + p.wid)
+        .then(res => res.json())
+        .then((result) => {
+          return {wid: p.wid, product: result.name.trim(), price: result.offers.price, usualprice: p.price}
+        })
+      })
+  
+    Promise.all(promises).then(results => {
+      setAlerts(results)
+    })
+  }
+
+  fetchAllWoolisProducts()
+
+
   return (
     <div>
       <IconButton href='/' edge="start" className={classes.backButton} color="inherit" aria-label="menu">
@@ -14,8 +51,8 @@ const Alerts = ({ alerts }) => {
       </IconButton>
       <MUList className={classes.list}>
         {alerts.map((alert) => (
-          <Fade in={true} key={alert.product}>
-              <AlertListItem id={alert.product} product={alert.product} price={alert.price} usualprice={alert.usualprice}/>
+          <Fade in={true} key={alert.wid}>
+              <AlertListItem id={alert.id} product={alert.product} price={alert.price} usualprice={alert.usualprice}/>
           </Fade>
         ))}
       </MUList>
