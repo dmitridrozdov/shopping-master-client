a760c097c560b21f3ff7a32376eb793dfbb18918
dmitridrozdov <dm.drozdov@gmail.com>
2022-11-02 13:20:04 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,43 +5,52 @@ import { Container } from '@material-ui/core'
 import AppList from './components/AppList/AppList'
 import Alerts from './components/Alerts/Alerts'
 
-const woolisApi = {
-    base: 'https://www.woolworths.com.au/api/v3/ui/schemaorg/product/',
-}
+// const woolisApi = {
+//     base: 'https://www.woolworths.com.au/api/v3/ui/schemaorg/product/',
+// }
 
 const productsToMonitor = [
     { 
         wid: '231598',
-        price: '19'
-    }
+        price: '18',
+    },
+    {
+        wid: '160945',
+        price: '26',
+    },
+    { 
+        wid: '813898',
+        price: '5',
+    },
 ]
 
 const App = () => {
 
-    let alertArray = []
-    const [alerts, setAlerts] = useState([])
+    // let alertArray = []
+    // const [alerts, setAlerts] = useState([])
 
-    useEffect(() => {
-        productsToMonitor.forEach(p => 
-            fetch(`${woolisApi.base}` + p.wid)
-              .then((res) => res.json())
-              .then((result) => {
-                if(p.price > result.offers.price)
-                {
-                    alertArray.push({product: result.name.trim(), price: result.offers.price, usualprice: p.price})
-                    setAlerts(alertArray)
-                }
-              })
-        )
+    // useEffect(() => {
+    //     productsToMonitor.forEach(p => 
+    //         fetch(`${woolisApi.base}` + p.wid)
+    //           .then((res) => res.json())
+    //           .then((result) => {
+    //             // if(p.price > result.offers.price) //uncomment if you need to push only when the price down
+    //             // {
+    //                 alertArray.push({wid: p.wid, product: result.name.trim(), price: result.offers.price, usualprice: p.price})
+    //                 // alertArray.push(result.name.trim())
+    //                 setAlerts(alertArray)
+    //             // }
+    //           })
+    //     )
         
-    }, [productsToMonitor])
+    // }, [productsToMonitor])
 
     return (
         <BrowserRouter>
             <Container maxWidth='xl'>
                 <Routes>
                     <Route path='/' exact element={<AppList />} />
-                    <Route path='/alerts' exact element={<Alerts alerts={alerts}/>} />
+                    <Route path='/alerts' exact element={<Alerts />} />
                 </Routes>
             </Container>
         </BrowserRouter>
