e20b4465f68a1825a70a725edfaeae16a89c275d
dmitridrozdov <dm.drozdov@gmail.com>
2022-11-03 16:56:51 +1100

    Update Alerts.js

--- src/components/Alerts/Alerts.js
+++ src/components/Alerts/Alerts.js
@@ -4,46 +4,10 @@ import { Fade, List as MUList, IconButton } from '@material-ui/core'
 import ArrowBack from '@material-ui/icons/ArrowBackIosOutlined'
 import AlertListItem from './AlertListItem/AlertListItem'
 
-const woolisApi = {
-  base: 'https://www.woolworths.com.au/api/v3/ui/schemaorg/product/',
-}
 
-const productsToMonitor = [
-  { 
-      wid: '231598',
-      price: '18',
-  },
-  {
-      wid: '160945',
-      price: '26',
-  },
-  { 
-      wid: '813898',
-      price: '5',
-  },
-]
-
-const Alerts = () => {
+const Alerts = ({ alerts }) => {
   const classes = useStyles()
-  const [alerts, setAlerts] = useState([])
-
-  const fetchAllWoolisProducts = () => {
-    const promises = productsToMonitor.map(p => {
-      return fetch(`${woolisApi.base}` + p.wid)
-        .then(res => res.json())
-        .then((result) => {
-          return {wid: p.wid, product: result.name.trim(), price: result.offers.price, usualprice: p.price}
-        })
-      })
   
-    Promise.all(promises).then(results => {
-      setAlerts(results)
-    })
-  }
-
-  fetchAllWoolisProducts()
-
-
   return (
     <div>
       <IconButton href='/' edge="start" className={classes.backButton} color="inherit" aria-label="menu">
