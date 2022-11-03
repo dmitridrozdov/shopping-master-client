fb67bd3c8771dccf744319f31023e197c7774672
dmitridrozdov <dm.drozdov@gmail.com>
2022-11-03 16:56:46 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Container } from '@material-ui/core'
 
 import AppList from './components/AppList/AppList'
 import Alerts from './components/Alerts/Alerts'
 
-// const woolisApi = {
-//     base: 'https://www.woolworths.com.au/api/v3/ui/schemaorg/product/',
-// }
+const woolisApi = {
+    base: 'https://www.woolworths.com.au/api/v3/ui/schemaorg/product/',
+}
 
 const productsToMonitor = [
     { 
@@ -26,31 +26,30 @@ const productsToMonitor = [
 
 const App = () => {
 
-    // let alertArray = []
-    // const [alerts, setAlerts] = useState([])
+    const [alerts, setAlerts] = useState([])
 
-    // useEffect(() => {
-    //     productsToMonitor.forEach(p => 
-    //         fetch(`${woolisApi.base}` + p.wid)
-    //           .then((res) => res.json())
-    //           .then((result) => {
-    //             // if(p.price > result.offers.price) //uncomment if you need to push only when the price down
-    //             // {
-    //                 alertArray.push({wid: p.wid, product: result.name.trim(), price: result.offers.price, usualprice: p.price})
-    //                 // alertArray.push(result.name.trim())
-    //                 setAlerts(alertArray)
-    //             // }
-    //           })
-    //     )
-        
-    // }, [productsToMonitor])
+    const fetchAllWoolisProducts = () => {
+      const promises = productsToMonitor.map(p => {
+        return fetch(`${woolisApi.base}` + p.wid)
+          .then(res => res.json())
+          .then((result) => {
+            return {wid: p.wid, product: result.name.trim(), price: result.offers.price, usualprice: p.price}
+          })
+        })
+    
+      Promise.all(promises).then(results => {
+        setAlerts(results)
+      })
+    }
+  
+    fetchAllWoolisProducts()
 
     return (
         <BrowserRouter>
             <Container maxWidth='xl'>
                 <Routes>
                     <Route path='/' exact element={<AppList />} />
-                    <Route path='/alerts' exact element={<Alerts />} />
+                    <Route path='/alerts' exact element={<Alerts alerts={alerts}/>} />
                 </Routes>
             </Container>
         </BrowserRouter>
