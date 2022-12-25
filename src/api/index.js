18128a88470b910feea2565b2fd52ad1f7ae76f6
dmitridrozdov <dm.drozdov@gmail.com>
2022-12-26 08:56:13 +1100

    dd

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // const url = 'http://localhost:5000/currentlist'
 // const allProductsUrl = 'http://localhost:5000/products'
-const url = 'https://shopping-master-2.herokuapp.com/currentlist'
-const allProductsUrl = 'https://shopping-master-2.herokuapp.com/products'
+const url = 'https://shopping-master-server.adaptable.app/currentlist'
+const allProductsUrl = 'https://shopping-master-server.adaptable.app/products'
 
 export const fetchProducts = () => axios.get(url)
 export const createProduct = (product) => axios.post(url, product)
