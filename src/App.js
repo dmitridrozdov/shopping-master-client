ca0a22310778527b76d073d34afdf6e27d0dadf8
dmitridrozdov <dm.drozdov@gmail.com>
2023-11-18 10:39:14 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Container } from '@material-ui/core'
+import { parse } from 'node-html-parser'
 
 import AppList from './components/AppList/AppList'
 import Alerts from './components/Alerts/Alerts'
@@ -9,65 +10,82 @@ const woolisApi = {
     base: 'https://www.woolworths.com.au/api/v3/ui/schemaorg/product/',
 }
 
-const productsToMonitor = [
-    { 
-        wid: '231598',
-        price: '18',
-    },
-    {
-        wid: '160945',
-        price: '28',
-    },
-    { 
-        wid: '813898',
-        price: '5',
-    },
-]
+// const productsToMonitor = [
+//     { 
+//         wid: '231598',
+//         price: '18',
+//     },
+//     {
+//         wid: '160945',
+//         price: '28',
+//     },
+//     { 
+//         wid: '813898',
+//         price: '5',
+//     },
+// ]
 
 const products = [
   {
     product: 'Guylian Chocolate Seashells 250g',
     woolisid: '231598',
-    coleslink: 'https://www.coles.com.au/product/guylian-chocolate-seashells-250g-5235307'
+    coleslink: 'https://www.coles.com.au/product/guylian-chocolate-seashells-250g-5235307',
+    usualprice: '18',
   },
   {
     product: 'Lavazza Crema E Gusto Ground Coffee 1Kg',
     woolisid: '160945',
-    coleslink: ''
+    coleslink: '',
+    usualprice: '28',
   },
   {
-      product: 'Cadbury Old Gold Original Dark Chocolate Block 180g',
-      woolisid: '813898',
-      coleslink: 'https://www.coles.com.au/product/cadbury-old-gold-original-dark-chocolate-block-180g-2350568'
+    product: 'Cadbury Old Gold Original Dark Chocolate Block 180g',
+    woolisid: '813898',
+    coleslink: 'https://www.coles.com.au/product/cadbury-old-gold-original-dark-chocolate-block-180g-2350568',
+    usualprice: '6',
   }
 ]
 
 const App = () => {
 
-    const [alerts, setAlerts] = useState([])
+    const [woolisPrices, setWoolisPrices] = useState([])
+    const [colesPrices, setColesPrices] = useState([])
 
-    const fetchAllWoolisProducts = () => {
-      const promises = productsToMonitor.map(p => {
-        return fetch(`${woolisApi.base}` + p.wid)
+    const fetchAllWoolisPrices = () => {
+      const promises = products.map(p => {
+        return fetch(`${woolisApi.base}` + p.woolisid)
           .then(res => res.json())
           .then((result) => {
-            return {wid: p.wid, product: result.name.trim(), price: result.offers.price, usualprice: p.price}
+            return {wid: p.woolisid, product: p.product, price: result.offers.price, usualprice: p.usualprice}
           })
         })
     
       Promise.all(promises).then(results => {
-        setAlerts(results)
+        setWoolisPrices(results)
       })
     }
-  
-    fetchAllWoolisProducts()
+
+    const fetchColesPrices = async () => {
+      const res = await fetch('https://www.coles.com.au/product/cadbury-old-gold-original-dark-chocolate-block-180g-2350568')
+      const html = await res.text()
+      
+      const root = parse(html)
+      const selectedElement = root.querySelector('.price__value')
+      if(selectedElement) {
+        const price = selectedElement.textContent
+        console.log('Coles price = ', price)
+      }
+    }
+
+    fetchColesPrices()
+    fetchAllWoolisPrices()
 
     return (
         <BrowserRouter>
             <Container maxWidth='xl'>
                 <Routes>
                     <Route path='/' exact element={<AppList />} />
-                    <Route path='/alerts' exact element={<Alerts alerts={alerts}/>} />
+                    <Route path='/alerts' exact element={<Alerts alerts={woolisPrices}/>} />
                 </Routes>
             </Container>
         </BrowserRouter>
