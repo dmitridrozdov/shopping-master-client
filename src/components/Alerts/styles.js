e27918f52218c214ee3bec83fb66e02c2d40c6d9
dmitridrozdov <dm.drozdov@gmail.com>
2023-11-20 16:42:05 +1100

    Update styles.js

--- src/components/Alerts/styles.js
+++ src/components/Alerts/styles.js
@@ -2,5 +2,18 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
 
+    product: {
+        fontFamily: 'Arvo', 
+        fontSize: '12px',
+    },
+
+    priceNormal: {
+        fontFamily: 'Montserrat', 
+        fontSize: '11px',
+    },
+
+    priceContainer: {
+        marginTop: theme.spacing(1),
+    },
 
 }));
