3fb4e3ec00d6dc727e0c28217c93b05e9b071af5
dmitridrozdov <dm.drozdov@gmail.com>
2023-11-20 16:42:02 +1100

    Update ProductCard.js

--- src/components/Alerts/ProductCard.js
+++ src/components/Alerts/ProductCard.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core/'
+import useStyles from './styles'
 
 const ProductCard = ({ product, price, usualprice, colesprice }) => {
+  const classes = useStyles()
+
   return (
     <Card>
       <CardContent>
-        <Typography variant="h5" component="div">
+        <Typography className={classes.product} variant="h5" component="div">
           {product}
         </Typography>
-        <Typography variant="subtitle1" color="text.secondary">
-          Price: {price}
-        </Typography>
-        <Typography variant="subtitle1" color="text.secondary">
-          Usual Price: {usualprice}
-        </Typography>
-        <Typography variant="subtitle1" color="text.secondary">
-          Coles Price: {colesprice}
-        </Typography>
+        <div className={classes.priceContainer}>
+            <Typography className={classes.priceNormal} variant="subtitle1" color="text.secondary">
+                Woolis Price: {price}
+            </Typography>
+            <Typography className={classes.priceNormal} variant="subtitle1" color="text.secondary">
+                Coles Price: {colesprice}
+            </Typography>
+            <Typography className={classes.priceNormal} variant="subtitle1" color="text.secondary">
+                Usual Price: {usualprice}
+            </Typography>
+        </div>
       </CardContent>
     </Card>
   );
